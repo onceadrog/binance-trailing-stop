3a0ffb8c612cc604dece535221b9bc95d8ca21f8
Mike <mexikiwimike@gmail.com>
2019-12-12 10:36:28 -0600

    Bug fix
    Arrays should start at 0 for keys to kingdom

--- binance_tp.py
+++ binance_tp.py
@@ -11,8 +11,8 @@ from datetime import datetime
 
 from binance_api import Binance
 
-API_KEY=api_key.API_ACCESS[1]
-API_SECRET=api_key.API_ACCESS[2]
+API_KEY=api_key.API_ACCESS[0]
+API_SECRET=api_key.API_ACCESS[1]
 
 bot = Binance(
     API_KEY,
