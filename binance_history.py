745218d6e1ad26e9d6d5038df4f2f1fcd157acd5
Mike <mexikiwimike@gmail.com>
2019-12-12 11:28:09 -0600

    CSV logging
    Removed redundant code and tidied inputs to query. Added CSV output and tidy headers.

--- binance_history.py
+++ binance_history.py
@@ -1,8 +1,5 @@
-"""
-    Detailed information about the bot on bablofil.ru/bot-dlya-binance
-"""
-import sqlite3
-import logging
+#import sqlite3    #Not required for this, but maybe in future to save to database instead
+#import logging    #Not required yet, but maybe for logging on a bigger function
 import time
 import os
 import math
@@ -12,43 +9,42 @@ from datetime import datetime
 from binance_api import Binance
 import api_key
 
+#Set up variables
 API_KEY=api_key.API_ACCESS[0]
 API_SECRET=api_key.API_ACCESS[1]
-
+filename = 'hist.csv'
+history_header=['Open time (milliseconds)',
+                'Open',
+                'High',
+                'Low',
+                'Close',
+                'Volume',
+                'Close time (milliseconds)',
+                'Quote Asset Volume',
+                '# Trades',
+                'Taker buy base asset volume',
+                'Taker buy quote asset volume',
+                'Ignore']
 bot = Binance(
     API_KEY,
     API_SECRET
 )
+hist_symbol='ETHUSDT'
+hist_interval='1m'
+dateTimeObj = datetime.now()
+timestampStr = dateTimeObj.strftime("%d-%b-%Y (%H:%M)")
+history_settings=f"Retrieved market data for {hist_symbol} at intervals of {hist_interval} as at {timestampStr}"
 
-settings = dict(
-    symbol='BNBBTC',            # A pair for tracking
-    strategy="Short",           # Strategy - Long (up), Short (down)           
-    stop_loss_perc = 0.5,       # % remaining from the price
-    stop_loss_fixed = 0,        # The initial stop-loss can be set by hand, then the bot will tighten.
-                                # You can specify 0, then the bot will calculate, take the current price and apply a percentage to it
-    amount = 0.0015             # Number of coins we plan to sell (in case of Long) or buy (in case of Short)
-                                # If we specify Long, then the violas are for sale (e.g., sell 0.1 ETH in a pair of ETHBTCs)
-                                # If Short, then the amount of money to buy, for example, to buy at 0.1 BTC on ETHBTC pair
-)
-
-multiplier = -1 if settings['strategy'] == "Long" else 1
-
-print("Getting pairs' settings from the stock exchange")
-symbols = bot.exchangeInfo()['symbols']
-step_sizes = {symbol['symbol']:symbol for symbol in symbols}
-for symbol in symbols:
-    for f in symbol['filters']:
-        if f['filterType'] == 'LOT_SIZE':
-            step_sizes[symbol['symbol']] = float(f['stepSize'])
-
+#Get the data
 print('Retrieving market data for ETHUSDT')
-
-res = bot.klines(symbol='ETHUSDT',
-                     interval='1m',
+res = bot.klines(symbol=hist_symbol',
+                     interval=hist_interval,
                      limit='1000')
+
+#Output on screen (compressed block) and save to csv file
 print('Market Result',res)
-
-#linebreaks
-#strip characters
-#append to textstream object
-
+with open(filename, 'w', newline='\n') as myfile:
+     wr = csv.writer(myfile)
+     wr.writerow([history_settings])    #[] wraps string in a list so it doesn't delimit the string as an array of characters
+     wr.writerow(history_header)
+     wr.writerows(res)
